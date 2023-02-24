6f4b3297733717e163004ea1e1deb941fade2c8c
Monica Navas <54577@novasbe.pt>
2023-02-24 15:17:19 +0000

    data_EDA branch

---
+++ agros.py
@@ -0,0 +1,49 @@
+"""
+The OS module in Python provides a way of using operating system dependent functionality.
+"""
+import os
+import urllib.request
+import pandas as pd
+
+
+class Agros:
+    """
+    A class for downloading and processing agricultural data.
+    """
+
+    def __init__(self):
+        """
+        Initializes the Agros class.
+        """
+        self.url = "https://raw.githubusercontent.com/owid/owid-datasets/master/datasets/Agricultural%20total%20factor%20productivity%20(USDA)/Agricultural%20total%20factor%20productivity%20(USDA).csv"
+        self.filename = "Agricultural total factor productivity (USDA).csv"
+        self.download_dir = os.path.join(os.getcwd(), "downloads")
+        self.download_path = os.path.join(self.download_dir, self.filename)
+        self.dataset = None
+
+    def download_data(self):
+        """
+        Downloads data from a given URL and saves it to a specified file path.
+        
+        Returns:
+            pandas.DataFrame: The downloaded dataset.
+        """
+        if os.path.isfile(self.download_path):
+            print("Data file already exists, skipping download...")
+            self.dataset = pd.read_csv(
+                "downloads/Agricultural total factor productivity (USDA).csv"
+            )
+            return self.dataset
+
+        if not os.path.exists(self.download_dir):
+            os.makedirs(self.download_dir)
+            print(f"Created directory: {self.download_dir}")
+
+        print(f"Downloading data file from {self.url}...")
+        urllib.request.urlretrieve(self.url, self.download_path)
+
+        print(f"Data file downloaded and saved to {self.download_path}")
+        self.dataset = pd.read_csv(
+            "downloads/Agricultural total factor productivity (USDA).csv"
+        )
+        return self.dataset
